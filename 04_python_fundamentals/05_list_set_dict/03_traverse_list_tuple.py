b8d3867f3af27d4a9224d3206e3873e88a69fe2c
Anon Bianco <anonh7@gmail.com>
2025-01-05 23:02:30 +0530

    add

---
+++ 04_python_fundamentals/05_list_set_dict/03_traverse_list_tuple.py
@@ -0,0 +1,24 @@
+fruits_list = ["apple", "mango", "orange", "pineapple"]
+
+# Method 1 
+for i in range(len(fruits_list)):
+  print(fruits_list[i])
+print()
+
+# Method 2
+for i in fruits_list:
+  print(i)
+print()
+
+# Traverse Tupple
+fruits_tupple = ("apple", "mango", "orange", "pineapple")
+
+# Method 1
+for i in range(len(fruits_tupple)):
+  print(fruits_tupple[i])
+print()
+
+# Method 2
+for i in fruits_tupple:
+  print(i)
+print()  
---
+++ 04_python_fundamentals/05_list_set_dict/04_slicing.py
@@ -0,0 +1,16 @@
+# String slicing
+name = "Anon Bianco"
+
+print(name[0:3])
+
+print(name[0:10:2])
+
+# List slicing
+fruits_list = ["apple", "mango", "orange", "pineapple"]
+fruits_list_2 = fruits_list[0:2]
+print(fruits_list_2)
+
+# Tuple slicing
+fruits_tupple = ("apple", "mango", "orange", "pineapple")
+fruits_tupple_2 = fruits_tupple[0:3]
+print(fruits_tupple_2)
