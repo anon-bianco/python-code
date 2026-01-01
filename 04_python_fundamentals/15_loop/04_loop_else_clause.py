d0cb29dab2c1d2b52f3bf175e0b9c1c3f0ef0362
Anon Bianco <anon.bianco@synbrains.ai>
2026-01-01 22:22:13 +0530

    update

---
+++ 04_python_fundamentals/15_loop/04_loop_else_clause.py
@@ -0,0 +1,14 @@
+for i in range(5):
+  print(i)
+else:
+  print('Else clause is executed')
+
+print("---case 2---")  
+
+
+for i in range(5):
+  if i==3:
+    break
+  print(i)
+else:
+  print('Else clause is executed')  
---
+++ 04_python_fundamentals/15_loop/05_rough.py
@@ -0,0 +1,7 @@
+lst = [10, 30, 40, 60]
+
+for i in lst:
+    if i == 40:
+        break
+else:
+    print('40')    
